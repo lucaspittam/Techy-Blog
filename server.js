e61164b36a141734d7d31fc8a20145a7887bad7e
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-05-27 10:23:00 -0400

    created handlebars pages and addded connection to sequelize data

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+const exphbs = require('express-handlebars');
+
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Handlebars template
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
+//Connection to server
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
