7fcb71c3d4a709617ebdb809b2df4da4fd63b722
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-05-29 13:16:32 -0400

    fixed error and added help functions

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/help');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 require('dotenv').config();
@@ -28,12 +29,14 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 
+//set handlebars as template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
+//turn on routes
 app.use(routes);
 
+//turn on connection to db and server
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
