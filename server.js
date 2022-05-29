26b4c900427d09d28f62372cf8b5c7886f3f4fa0
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-05-29 16:22:15 -0400

    final touches

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const sess = {
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
