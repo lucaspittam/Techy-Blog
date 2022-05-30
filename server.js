82562ff3f0577c7d4ddc8f026692e75dd38caecf
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-05-29 21:18:07 -0400

    heroku error

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const sess = {
 
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
